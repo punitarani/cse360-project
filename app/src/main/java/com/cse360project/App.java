19caeee28b094bfdf3031ec28008b7855e591beb
Punit Arani <punitsai36@gmail.com>
2022-11-09 17:20:03 -0700

    Implement app.fxml

--- app/src/main/java/com/cse360project/App.java
+++ app/src/main/java/com/cse360project/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,19 +15,54 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    public static Pane mainPane;
+    public static Pane headerPane;
 
+    /**
+     * Starts the application
+     *
+     * @param stage the primary stage for this application, onto which the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be primary stages.
+     *
+     * @throws IOException if any of the FXML files cannot be loaded.
+     */
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("app"), 640, 480);
         stage.setTitle("ASU Group 48 Pizza Kitchen");
         stage.setScene(scene);
         stage.show();
+
+        loadComponents();
+        setMainPane("primary");
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+    /**
+     * Load the components of the app
+     */
+    public static void loadComponents() {
+        mainPane = (Pane) scene.lookup("#mainPane");
+        headerPane = (Pane) scene.lookup("#headerPane");
     }
 
+    /**
+     * Set the main pane (#mainPane in app.fxml)
+     * @param fxml The fxml file to load
+     * @throws IOException If the fxml file is not found
+     */
+    static void setMainPane(String fxml) throws IOException {
+        mainPane.getChildren().clear();
+        mainPane.getChildren().add(loadFXML(fxml));
+    }
+
+    /**
+     * Load the FXML file
+     *
+     * @param fxml FXML file to load (relative to app\src\main\resources\com.cse360project\ and without .fxml extension)
+     * @return Parent object
+     *
+     * @throws IOException if the file cannot be found
+     */
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
