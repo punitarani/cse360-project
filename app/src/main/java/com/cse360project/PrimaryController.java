53c2fdb798197cc5d1434c32464214a7b3e9cecf
Punit Arani <punitsai36@gmail.com>
2022-11-11 19:00:24 -0700

    Update switchToSecondary to setMainPane to pizzaBuilder

--- app/src/main/java/com/cse360project/PrimaryController.java
+++ app/src/main/java/com/cse360project/PrimaryController.java
@@ -8,6 +8,6 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setMainPane("secondary");
+        App.setMainPane("pizzaBuilder");
     }
 }
