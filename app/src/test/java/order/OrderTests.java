7b3dc40ae6c66bedf0b6ce0de1ca11e88f44447c
Punit Arani <punitsai36@gmail.com>
2022-11-12 14:09:51 -0700

    Add getTotal method and tests

--- app/src/test/java/order/OrderTests.java
+++ app/src/test/java/order/OrderTests.java
@@ -12,6 +12,7 @@ class OrderTests {
     void testOrderInit() {
         assertEquals(10, order.getId().length());
         assertEquals(0, order.getItems().size());
+        assertEquals(0, order.getTotal());
         assertEquals(OrderStatus.PENDING, order.getStatus());
     }
 
@@ -26,6 +27,7 @@ class OrderTests {
         Pizza pizza = new Pizza();
         order.addItem(pizza);
         assertEquals(1, order.getItems().size());
+        assertEquals(pizza.getPrice(), order.getTotal());
         assertEquals(pizza, order.getItems().get(0));
     }
 
@@ -34,7 +36,15 @@ class OrderTests {
         Pizza pizza = new Pizza();
         order.addItem(pizza);
         assertEquals(1, order.getItems().size());
+        assertEquals(pizza.getPrice(), order.getTotal());
+        order.addItem(pizza);
+        assertEquals(2, order.getItems().size());
+        assertEquals(pizza.getPrice() * 2, order.getTotal());
+        order.removeItem(pizza);
+        assertEquals(1, order.getItems().size());
+        assertEquals(pizza.getPrice(), order.getTotal());
         order.removeItem(pizza);
         assertEquals(0, order.getItems().size());
+        assertEquals(0, order.getTotal());
     }
 }
