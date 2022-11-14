832be42ef07d8eb37af1defd6f5a3dc570bdfe98
xxTotalWarxx <jrodi4m@gmail.com>
2022-11-13 17:28:05 -0700

    Login Page
    Fix the page by adding and error message. In the controller, it verifies if  it is a valid asurite.

--- app/src/main/java/com/cse360project/LoginPageController.java
+++ app/src/main/java/com/cse360project/LoginPageController.java
@@ -1,13 +1,18 @@
 package com.cse360project;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginPageController {
-    public TextField idField;
+    //public TextField idField;
     public Button customerButton;
     public Button chefButton;
 
+    public Label errorMessage;
+    public PasswordField idField;
+    private String asurite;
     public void initialize() {
         // Add event handlers
         customerButton.setOnAction(event -> customerButtonHandler());
@@ -15,10 +20,31 @@ public class LoginPageController {
     }
 
     private void customerButtonHandler() {
-        App.setMainPane("pizzaBuilder");
+        asurite = idField.getText();
+
+        if(asurite.charAt(0) == '1' && asurite.length() == 10){
+            App.setMainPane("pizzaBuilder");
+        }else{
+            //Throw error message
+            errorMessage.setText("Error: ASURITE NOT VALID");
+        }
+
     }
 
     private void chefButtonHandler() {
-        App.setMainPane("pizzaManager");
+
+        asurite = idField.getText();
+
+        if(asurite.charAt(0) == '1' && asurite.length() == 10){
+            App.setMainPane("pizzaManager");
+        }else{
+            //Throw error message
+            errorMessage.setText("Error: ASURITE NOT VALID");
+        }
     }
+
+    public String getAsurite(){
+        return asurite;
+    }
+
 }
